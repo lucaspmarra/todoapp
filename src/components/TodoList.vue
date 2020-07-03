3193bafda1fb8221c5d8437a4a0c97a41e414fad
Lucas Marra <marralucas1@gmail.com>
2020-07-02 22:39:41 -0300

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <label for="todo-item" hidden>O que precisa ser feito?</label>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="O que precisa ser feito?"
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+    />
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed" />
+        <input type="checkbox" @click="editTodo(todo)" />
+        <div
+          v-if="!todo.editing"
+          @dblclick="editTodo(todo)"
+          class="todo-item-label"
+          :class="{completed:todo.completed}"
+        >{{todo.title}}</div>
+        <input
+          v-else
+          class="todo-item-edit"
+          type="text"
+          v-model="todo.title"
+          @blur="doneEdit(todo)"
+          @keyup.enter="doneEdit(todo)"
+          @keyup.esc="cancelEdit(todo)"
+          v-focus
+        />
+      </div>
+
+      <div class="remove-item" @click="removeTodo(index)">&times;</div>
+    </div>
+    <div class="extra-container">
+      <div>
+        <label for>
+          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos" />Marcar Todos
+        </label>
+      </div>
+      <div>{{remaining}} itens à completar</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+  data() {
+    return {
+      newTodo: "",
+      idForTodo: 3,
+      beforeEditCache: "",
+      todos: [
+        {
+          id: 1,
+          title: "Terminar série",
+          completed: false,
+          editing: false
+        },
+        {
+          id: 2,
+          title: "Estudar Vue.Js",
+          completed: false,
+          editing: false
+        }
+      ]
+    };
+  },
+  computed: {
+    remaining() {
+      return this.todos.filter(todo => !todo.completed).length;
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus();
+      }
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length == 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false
+      });
+      this.newTodo = "";
+      this.idForTodo++;
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title;
+      todo.editing = true;
+    },
+    doneEdit(todo) {
+      if (todo.title.trim().length == 0) {
+        todo.title = this.beforeEditCache;
+      }
+      todo.editing = false;
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache;
+      todo.editing = false;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  border-radius: 0.5rem;
+
+  &:focus {
+    outline: 0;
+  }
+}
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+  &:hover {
+    color: black;
+  }
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-label {
+  padding: 10px;
+  border: 1px solid white;
+  margin-left: 12px;
+}
+.todo-item-edit {
+  font-size: 16px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%px;
+  padding: 10px;
+  border: 1px solid #ccc; //override defaults
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  &:focus {
+    outline: none;
+  }
+}
+
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+.extra-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  border-top: 1px solid lightgrey;
+  padding-top: 14px;
+  margin-bottom: 14px;
+}
+.button {
+  font-size: 14px;
+  background-color: white;
+  appearance: none;
+  &:hover {
+    background: lightgreen;
+  }
+  &:focus {
+    outline: none;
+  }
+}
+.active {
+  background: lightgreen;
+}
+</style>
